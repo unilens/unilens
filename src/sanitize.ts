b2cfa12e57e1747f862f1254fdcd9a89e1536caa
Ari Weber <arimweber@gmail.com>
2026-04-19 17:09:39 -0400

    Fix sanitizing Style blocks

--- src/sanitize.ts
+++ src/sanitize.ts
@@ -5,12 +5,13 @@ export function sanitizePortfolio(dirty: string): string {
   return sanitizeHtml(dirty, {
     allowedTags: [
       'h1','h2','h3','p','br','hr','strong','em','u','s',
-      'ul','ol','li','a','img','blockquote','span','div','section'
+      'ul','ol','li','a','img','blockquote','span','div','section','style'
     ],
     allowedAttributes: {
-      'a':   ['href', 'title'],
-      'img': ['src', 'alt', 'width', 'height'],
-      '*':   ['class', 'style']
+      'a':     ['href', 'title'],
+      'img':   ['src', 'alt', 'width', 'height'],
+      'style': [],
+      '*':     ['class', 'style']
     },
     allowedStyles: {
       '*': {
@@ -43,6 +44,8 @@ export function sanitizePortfolio(dirty: string): string {
         'background-color': [/.*/]
       }
     },
+    allowedSchemesAppliedToAttributes: ['href', 'src'],
+    allowVulnerableTags: true,
     // Explicitly block everything dangerous <>
     disallowedTagsMode: 'discard'
   });
